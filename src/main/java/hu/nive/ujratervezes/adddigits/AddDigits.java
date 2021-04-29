f19f64f2584f30ba9a02681452a41464eba2b2ab
ViktorKOSZEGI <koszegiv.5@gmail.com>
2021-04-29 10:39:50 +0200

    Implement addDigits() method

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -2,4 +2,16 @@ package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
     //todo: implement your solution here
+    public int addDigits(String input) {
+        int sum = 0;
+        if (input.equals("") || input.equals(null)) {
+            return -1;
+        }
+        for (int i = 0; i < input.length(); i++) {
+            if(Character.isDigit(input.charAt(i))) {
+                sum += Integer.parseInt(String.valueOf(input.charAt(i)));
+            }
+        }
+        return sum;
+    }
 }
